7bf755b7e63b959ad68bfb56dfc1dd3c328a8549
Sneh Tekriwal <stekriwa@amazon.com>
2020-02-05 12:51:17 +0000

    Changed to a single module project. Will work with feature-package.

--- application/src/main/java/com/revolut/interview/application/AppStarter.java
+++
@@ -1,8 +0,0 @@
-package com.revolut.interview.application;
-
-public class AppStarter {
-
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/com/revolut/interview/TransferServiceStarter.java
@@ -0,0 +1,8 @@
+package com.revolut.interview;
+
+public class TransferServiceStarter {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
